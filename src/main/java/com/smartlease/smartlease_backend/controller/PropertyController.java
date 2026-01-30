5107f34718e1928f76b9ba067622002cac1a07fc
Akash9634 <akashkush2020@gmail.com>
2026-01-31 00:05:16 +0530

    request based on roles

--- src/main/java/com/smartlease/smartlease_backend/controller/PropertyController.java
+++ src/main/java/com/smartlease/smartlease_backend/controller/PropertyController.java
@@ -6,12 +6,15 @@ import com.smartlease.smartlease_backend.service.PropertyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAuthority;
 
 @RestController
 @RequestMapping("/api/v1/properties")
@@ -20,6 +23,7 @@ public class PropertyController {
     private final PropertyService service;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LANDLORD')")
     public ResponseEntity<?> saveProperty(@RequestBody PropertyRequest request, Principal connectedUser){
         service.saveProperty(request, connectedUser);
         return ResponseEntity.status(HttpStatus.CREATED).body("property saved successfully");
--- src/main/java/com/smartlease/smartlease_backend/model/Role.java
+++ src/main/java/com/smartlease/smartlease_backend/model/Role.java
@@ -1,7 +1,8 @@
 package com.smartlease.smartlease_backend.model;
 
 public enum Role {
-    ROLE_TENANT,
-    ROLE_LANDLORD,
-    ROLE_ADMIN
+    USER,
+    LANDLORD,
+    ADMIN,
+    TENANT
 }
