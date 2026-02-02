c296e2bd02e6c9cc633818a9da45618b75c16e3c
Akash9634 <akashkush2020@gmail.com>
2026-02-02 10:48:32 +0530

    updateproperty Controller implemented

--- src/main/java/com/smartlease/smartlease_backend/controller/PropertyController.java
+++ src/main/java/com/smartlease/smartlease_backend/controller/PropertyController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/properties")
@@ -23,4 +20,12 @@ public class PropertyController {
         service.saveProperty(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("property saved successfully");
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProperty(@RequestBody PropertyRequest request, @PathVariable Long id){
+        service.updateProperty(id, request);
+        return ResponseEntity.ok().body("property updated successfully");
+    }
+
+
 }
